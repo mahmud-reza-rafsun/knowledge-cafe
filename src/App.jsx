209ae22305d5ec918b878707896282a7f7bae579
Mahmud Reza Rafsun <97410422+mahmud-reza-rafsun@users.noreply.github.com>
2025-01-05 16:51:20 +0600

    bookmarked added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Blogs from "./components/Blogs/Blogs";
+import Bookmarks from "./components/Bookmarks/Bookmarks";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <Header></Header>
-      <Blogs></Blogs>
+      <div className="max-w-5xl mx-auto md:flex gap-10">
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import profile from "../../assets/images/profile.png";
 const Header = () => {
   return (
-    <div className="max-w-4xl mx-auto">
-      <div className="flex justify-between items-center p-4 mt-4 border-b">
+    <div className="max-w-5xl mx-auto">
+      <div className="flex justify-between items-center border-b mt-5 py-3">
         <h2 className="text-2xl font-bold">Knowledge Cafe</h2>
         <img src={profile} alt="" />
       </div>
