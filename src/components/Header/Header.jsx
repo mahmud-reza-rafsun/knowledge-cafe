1e8941f003745920f039f765be60c084a024b0e1
Mahmud Reza Rafsun <97410422+mahmud-reza-rafsun@users.noreply.github.com>
2025-01-05 12:56:50 +0600

    header section complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,11 @@
+import profile from "../../assets/images/profile.png";
 const Header = () => {
   return (
-    <div>
-      <h2 className="text-2xl font-bold">Knowledge Cafe</h2>
+    <div className="max-w-4xl mx-auto">
+      <div className="flex justify-between items-center p-4 mt-4 border-b">
+        <h2 className="text-2xl font-bold">Knowledge Cafe</h2>
+        <img src={profile} alt="" />
+      </div>
     </div>
   );
 };
